54a4d10d6d2a89ce258c3fdc94048da9608a8c77
CStank89 <C.Stank89@gmail.com>
2020-09-28 16:32:26 -0400

    working on setting state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import ToDo from "./components/Todo";
+import ToDoList from "./components/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +20,15 @@ class App extends React.Component {
       </div>
     );
   }
+
+  componentDidMount = () => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      console.log("has todos:", todos);
+    } else {
+      console.log("has no todos");
+    }
+  };
 }
 
 export default App;
